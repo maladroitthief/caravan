1ea70fa8381a394464686a91094a90267dc05c48
github-action-benchmark <github@users.noreply.github.com>
2024-06-14 13:11:09 +0000

    add Benchmark (go) benchmark result for 90b483e94cb5ddb0b782087d14df81338f065189

---
+++ dev/bench/data.js
@@ -0,0 +1,340 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1718370669258,
+  "repoUrl": "https://github.com/maladroitthief/caravan",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "maladroitthief",
+            "username": "maladroitthief"
+          },
+          "committer": {
+            "name": "maladroitthief",
+            "username": "maladroitthief"
+          },
+          "id": "90b483e94cb5ddb0b782087d14df81338f065189",
+          "message": "fix: Adding CI to the repo",
+          "timestamp": "2024-06-14T11:57:28Z",
+          "url": "https://github.com/maladroitthief/caravan/pull/1/commits/90b483e94cb5ddb0b782087d14df81338f065189"
+        },
+        "date": 1718370668831,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGenerationalIndexIndexing",
+            "value": 22.72,
+            "unit": "ns/op\t       5 B/op\t       0 allocs/op",
+            "extra": "44927744 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGenerationalIndexIndexing - ns/op",
+            "value": 22.72,
+            "unit": "ns/op",
+            "extra": "44927744 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGenerationalIndexIndexing - B/op",
+            "value": 5,
+            "unit": "B/op",
+            "extra": "44927744 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGenerationalIndexIndexing - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "44927744 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGenerationalIndexInsert",
+            "value": 31.49,
+            "unit": "ns/op\t     199 B/op\t       0 allocs/op",
+            "extra": "39490641 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGenerationalIndexInsert - ns/op",
+            "value": 31.49,
+            "unit": "ns/op",
+            "extra": "39490641 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGenerationalIndexInsert - B/op",
+            "value": 199,
+            "unit": "B/op",
+            "extra": "39490641 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGenerationalIndexInsert - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "39490641 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXPoolIndexing",
+            "value": 28.71,
+            "unit": "ns/op\t       5 B/op\t       0 allocs/op",
+            "extra": "50324270 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXPoolIndexing - ns/op",
+            "value": 28.71,
+            "unit": "ns/op",
+            "extra": "50324270 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXPoolIndexing - B/op",
+            "value": 5,
+            "unit": "B/op",
+            "extra": "50324270 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXPoolIndexing - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "50324270 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXInsert",
+            "value": 31.8,
+            "unit": "ns/op\t     187 B/op\t       0 allocs/op",
+            "extra": "42186654 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXInsert - ns/op",
+            "value": 31.8,
+            "unit": "ns/op",
+            "extra": "42186654 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXInsert - B/op",
+            "value": 187,
+            "unit": "B/op",
+            "extra": "42186654 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXInsert - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "42186654 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXPoolDelete",
+            "value": 0.5082,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXPoolDelete - ns/op",
+            "value": 0.5082,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXPoolDelete - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGIDXPoolDelete - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQEnqueue",
+            "value": 60.79,
+            "unit": "ns/op\t     165 B/op\t       0 allocs/op",
+            "extra": "17711940 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQEnqueue - ns/op",
+            "value": 60.79,
+            "unit": "ns/op",
+            "extra": "17711940 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQEnqueue - B/op",
+            "value": 165,
+            "unit": "B/op",
+            "extra": "17711940 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQEnqueue - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "17711940 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQDequeue",
+            "value": 0.6632,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQDequeue - ns/op",
+            "value": 0.6632,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQDequeue - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQDequeue - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQReverseEnqueue",
+            "value": 59.76,
+            "unit": "ns/op\t     163 B/op\t       0 allocs/op",
+            "extra": "18003432 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQReverseEnqueue - ns/op",
+            "value": 59.76,
+            "unit": "ns/op",
+            "extra": "18003432 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQReverseEnqueue - B/op",
+            "value": 163,
+            "unit": "B/op",
+            "extra": "18003432 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQReverseEnqueue - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "18003432 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQReverseDequeue",
+            "value": 0.6914,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQReverseDequeue - ns/op",
+            "value": 0.6914,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQReverseDequeue - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPQReverseDequeue - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPriorityQueueEnqueue",
+            "value": 120.6,
+            "unit": "ns/op\t      79 B/op\t       1 allocs/op",
+            "extra": "8938996 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPriorityQueueEnqueue - ns/op",
+            "value": 120.6,
+            "unit": "ns/op",
+            "extra": "8938996 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPriorityQueueEnqueue - B/op",
+            "value": 79,
+            "unit": "B/op",
+            "extra": "8938996 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPriorityQueueEnqueue - allocs/op",
+            "value": 1,
+            "unit": "allocs/op",
+            "extra": "8938996 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkReversePriorityQueueEnqueue",
+            "value": 126.8,
+            "unit": "ns/op\t      72 B/op\t       1 allocs/op",
+            "extra": "10354804 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkReversePriorityQueueEnqueue - ns/op",
+            "value": 126.8,
+            "unit": "ns/op",
+            "extra": "10354804 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkReversePriorityQueueEnqueue - B/op",
+            "value": 72,
+            "unit": "B/op",
+            "extra": "10354804 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkReversePriorityQueueEnqueue - allocs/op",
+            "value": 1,
+            "unit": "allocs/op",
+            "extra": "10354804 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPriorityQueueDequeue",
+            "value": 572.6,
+            "unit": "ns/op\t      43 B/op\t       0 allocs/op",
+            "extra": "1757403 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPriorityQueueDequeue - ns/op",
+            "value": 572.6,
+            "unit": "ns/op",
+            "extra": "1757403 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPriorityQueueDequeue - B/op",
+            "value": 43,
+            "unit": "B/op",
+            "extra": "1757403 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPriorityQueueDequeue - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1757403 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkReversePriorityQueueDequeue",
+            "value": 911.9,
+            "unit": "ns/op\t      68 B/op\t       0 allocs/op",
+            "extra": "1125381 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkReversePriorityQueueDequeue - ns/op",
+            "value": 911.9,
+            "unit": "ns/op",
+            "extra": "1125381 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkReversePriorityQueueDequeue - B/op",
+            "value": 68,
+            "unit": "B/op",
+            "extra": "1125381 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkReversePriorityQueueDequeue - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1125381 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
