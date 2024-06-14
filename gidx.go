a0bc547e7bd872478b63956acb1016bee8e0011d
Ian Weller <maladroitthief@gmail.com>
2024-06-14 07:57:08 -0400

    feat: GIDX Pool

--- gidx.go
+++ gidx.go
@@ -17,6 +17,18 @@ type (
 		entries []allocatorEntry
 		free    []int
 	}
+
+	GIDXArrayEntry[T any] struct {
+		value      T
+		generation int
+	}
+
+	GIDXArray[T any] []GIDXArrayEntry[T]
+
+	GIDXPool[T any] struct {
+		array     GIDXArray[T]
+		allocator *GIDXAllocator
+	}
 )
 
 func (g GIDX) Info() string {
@@ -82,19 +94,11 @@ func (g *GIDXAllocator) IsLive(i GIDX) bool {
 	return true
 }
 
-type GIDXArrayEntry[T any] struct {
-	value      T
-	generation int
-}
-
-type GIDXArray[T any] []GIDXArrayEntry[T]
-
 func NewGIDXArray[T any]() GIDXArray[T] {
 	return GIDXArray[T]{}
 }
 
 func (g GIDXArray[T]) Set(index GIDX, value T) GIDXArray[T] {
-
 	for len(g) <= index.index {
 		g = append(g, GIDXArrayEntry[T]{generation: -1})
 	}
@@ -102,15 +106,6 @@ func (g GIDXArray[T]) Set(index GIDX, value T) GIDXArray[T] {
 	g[index.index] = GIDXArrayEntry[T]{
 		value:      value,
 		generation: index.generation,
-	}
-
-	return g
-}
-
-func (g GIDXArray[T]) Remove(index GIDX) GIDXArray[T] {
-
-	if index.index < len(g) {
-		g[index.index].generation = -1
 	}
 
 	return g
@@ -131,7 +126,6 @@ func (g GIDXArray[T]) Get(index GIDX) T {
 }
 
 func (g GIDXArray[T]) GetAllIndices(a *GIDXAllocator) []GIDX {
-
 	result := []GIDX{}
 
 	for i, entry := range g {
@@ -166,7 +160,6 @@ func (g GIDXArray[T]) GetAll(a *GIDXAllocator) []T {
 }
 
 func (g GIDXArray[T]) First(a *GIDXAllocator) (GIDX, T) {
-
 	var defaultValue T
 	for i, entry := range g {
 		if entry.generation <= 0 {
@@ -180,4 +173,43 @@ func (g GIDXArray[T]) First(a *GIDXAllocator) (GIDX, T) {
 	}
 
 	return GIDX{}, defaultValue
+}
+
+func NewGIDXPool[T any]() *GIDXPool[T] {
+	return &GIDXPool[T]{
+		allocator: NewGIDXAllocator(),
+		array:     NewGIDXArray[T](),
+	}
+}
+
+func (p *GIDXPool[T]) IsLive(gidx GIDX) bool {
+	return p.allocator.IsLive(gidx)
+}
+
+func (p *GIDXPool[T]) Allocate() GIDX {
+	return p.allocator.Allocate()
+}
+
+func (p *GIDXPool[T]) Set(index GIDX, value T) {
+	p.array = p.array.Set(index, value)
+}
+
+func (p *GIDXPool[T]) Remove(index GIDX) {
+	p.allocator.Deallocate(index)
+}
+
+func (p *GIDXPool[T]) Get(index GIDX) T {
+	return p.array.Get(index)
+}
+
+func (p *GIDXPool[T]) First() (GIDX, T) {
+	return p.array.First(p.allocator)
+}
+
+func (p *GIDXPool[T]) GetAll() []T {
+	return p.array.GetAll(p.allocator)
+}
+
+func (p GIDXPool[T]) GetAllIndices() []GIDX {
+	return p.array.GetAllIndices(p.allocator)
 }
--- gidx_test.go
+++ gidx_test.go
@@ -43,24 +43,80 @@ func BenchmarkGenerationalIndexInsert(b *testing.B) {
 	}
 }
 
-func BenchmarkGenerationalIndexDelete(b *testing.B) {
+// func BenchmarkGenerationalIndexDelete(b *testing.B) {
+// 	type entity struct {
+// 		id int
+// 	}
+// 	genIndexAllocator := *caravan.NewGIDXAllocator()
+// 	genIndexArray := caravan.NewGIDXArray[entity]()
+// 	indexIds := []caravan.GIDX{}
+// 	for i := 0; i < ContainerSize; i++ {
+// 		entityId := genIndexAllocator.Allocate()
+// 		indexIds = append(indexIds, entityId)
+// 		genIndexArray = genIndexArray.Set(entityId, entity{id: rand.Int()})
+// 	}
+
+// 	for n := 0; n < b.N; n++ {
+// 		if n < ContainerSize {
+// 			index := rand.Intn(len(indexIds))
+
+// 			genIndexArray = genIndexArray.Remove(indexIds[index])
+// 			ge.Remove(indexIds[index])
+
+// 			indexIds[index] = indexIds[len(indexIds)-1]
+// 			indexIds = indexIds[:len(indexIds)-1]
+// 		}
+// 	}
+// }
+
+func BenchmarkGIDXPoolIndexing(b *testing.B) {
 	type entity struct {
 		id int
 	}
-	genIndexAllocator := *caravan.NewGIDXAllocator()
-	genIndexArray := caravan.NewGIDXArray[entity]()
+	pool := *caravan.NewGIDXPool[entity]()
 	indexIds := []caravan.GIDX{}
 	for i := 0; i < ContainerSize; i++ {
-		entityId := genIndexAllocator.Allocate()
-		indexIds = append(indexIds, entityId)
-		genIndexArray = genIndexArray.Set(entityId, entity{id: rand.Int()})
+		id := pool.Allocate()
+		indexIds = append(indexIds, id)
+		pool.Set(id, entity{id: rand.Int()})
+	}
+
+	for n := 0; n < b.N; n++ {
+		_ = pool.Get(indexIds[rand.Intn(ContainerSize)])
+	}
+}
+
+func BenchmarkGIDXInsert(b *testing.B) {
+	type entity struct {
+		id int
+	}
+
+	pool := *caravan.NewGIDXPool[entity]()
+	var id caravan.GIDX
+	for n := 0; n < b.N; n++ {
+		id = pool.Allocate()
+		pool.Set(id, entity{id: rand.Int()})
+	}
+}
+
+func BenchmarkGIDXPoolDelete(b *testing.B) {
+	type entity struct {
+		id int
+	}
+
+	pool := *caravan.NewGIDXPool[entity]()
+	indexIds := []caravan.GIDX{}
+	for i := 0; i < ContainerSize; i++ {
+		id := pool.Allocate()
+		indexIds = append(indexIds, id)
+		pool.Set(id, entity{id: rand.Int()})
 	}
 
 	for n := 0; n < b.N; n++ {
 		if n < ContainerSize {
 			index := rand.Intn(len(indexIds))
 
-			genIndexArray = genIndexArray.Remove(indexIds[index])
+			pool.Remove(indexIds[index])
 
 			indexIds[index] = indexIds[len(indexIds)-1]
 			indexIds = indexIds[:len(indexIds)-1]
